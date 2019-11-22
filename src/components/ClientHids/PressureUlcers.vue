8a4344af70cecd21846eeb6f976a51df904c76cf
Lucille Anne Teves <lucilleanneteves@gmail.com>
2019-11-22 21:12:59 +0800

    hids

--- src/components/ClientHids/PressureUlcers.vue
+++ src/components/ClientHids/PressureUlcers.vue
@@ -3,6 +3,69 @@
         <v-container>
             <v-row>
                 <v-col cols="12" sm="12" md="12">
+                    <div class="subtitle-1"><span class="font-weight-bold">Pressure Ulcer Stages</span></div>
+                </v-col>
+                <v-col cols="12" sm="6" md="9">
+                    <div class="subtitle-1"><span class="font-weight-bold">Stage 1:</span> Nonblanchable erythema of intact skin; the heralding of skin ulceration. In dark-pigmented skin, warmth, edema, hardness or discolored skin may be indicators.</div>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-combobox
+                        v-model="pu.pu1"
+                        :items="['1','2','3 or more']"
+                        label="Number of Pressure Ulcers"
+                        single    
+                        chips
+                    ></v-combobox>
+                </v-col>
+                <v-col cols="12" sm="6" md="9">
+                    <div class="subtitle-1"><span class="font-weight-bold">Stage 2:</span> Partial thickness skin loss involving epidermis and/or dermis. The ulcer is superficial and presents clinically as an abrasion, blister, or shallow crater.</div>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-combobox
+                        v-model="pu.pu2"
+                        :items="['1','2','3 or more']"
+                        label="Number of Pressure Ulcers"
+                        single    
+                        chips
+                    ></v-combobox>
+                </v-col>
+                <v-col cols="12" sm="6" md="9">
+                    <div class="subtitle-1"><span class="font-weight-bold">Stage 3:</span> Full-thickness skin loss involving damage or necrosis of subcutaneous tissue which may extend down to,but not through, underlying fascia. The ulcer presents clinically as a deep crater with or without undermining of adjacent tissue.</div>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-combobox
+                        v-model="pu.pu3"
+                        :items="['1','2','3 or more']"
+                        label="Number of Pressure Ulcers"
+                        single    
+                        chips
+                    ></v-combobox>
+                </v-col>
+                <v-col cols="12" sm="6" md="9">
+                    <div class="subtitle-1"><span class="font-weight-bold">Stage 4:</span> Full-thickness skin loss with extensive destruction, tissue necrosis,or damage to muscle, bone, or supporting structures (e.g. tendon, joint capsule, etc.)</div>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-combobox
+                        v-model="pu.pu4"
+                        :items="['1','2','3 or more']"
+                        label="Number of Pressure Ulcers"
+                        single    
+                        chips
+                    ></v-combobox>
+                </v-col>
+                <v-col cols="12" sm="6" md="9">
+                    <div class="subtitle-1">In addition to the above, is there at least one pressure ulcer that cannot be staged or observed due to the presence of eschar or a non-removable dressing, including casts?</div>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <v-combobox
+                        v-model="pu.pu5"
+                        :items="['Yes', 'No']"
+                        label="Yes / No"
+                        single    
+                        chips
+                    ></v-combobox>
+                </v-col>
+                <!--<v-col cols="12" sm="12" md="12">
                     <v-combobox
                         v-model="pu.pu1"
                         :items="['Patient lives alone',
@@ -12,7 +75,7 @@
                         single    
                         chips
                     ></v-combobox>
-                </v-col>
+                </v-col>-->
             </v-row>
         </v-container>
     </div>
--- src/components/ClientHids/WoundDescription.vue
+++ src/components/ClientHids/WoundDescription.vue
@@ -2,16 +2,121 @@
     <div>
         <v-container>
             <v-row>
-                <v-col cols="12" sm="12" md="12">
+                <v-col cols="12" sm="6" md="6" >
+                    <div class="subtitle-1">Measurements (L x W x Depth):</div>
+                </v-col>
+                <v-col cols="12" sm="2" md="2">
+                    <v-text-field
+                        v-model="wd.wdl"
+                        label="(cm)"
+                        suffix="x"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="2" md="2">
+                    <v-text-field
+                        v-model="wd.wdw"
+                        label="(cm)"
+                        suffix="x"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="2" md="2">
+                    <v-text-field
+                        v-model="wd.wdd"    
+                        label="(cm)"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="12" md="12" >
+                    <v-text-field
+                        v-model="wd.wd_location"
+                        label="Location"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" md="12" sm="12"><v-divider></v-divider></v-col>
+                <v-col cols="12" sm="4" md="4">
+                    <v-text-field
+                        v-model="wd.wd_granulation"
+                        label="Granulation"
+                        suffix="%"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="4" md="4">
+                    <v-text-field
+                        v-model="wd.wd_eschar"
+                        label="Eschar"
+                        suffix="%"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="4" md="4">
+                    <v-text-field
+                        v-model="wd.wd_slough"
+                        label="Slough"
+                        suffix="%"
+                        required>
+                    </v-text-field>
+                </v-col>
+                 <v-col cols="12" sm="6" md="6">
+                    <v-text-field
+                        v-model="wd.wd_tunneling"
+                        label="Tunneling"
+                        suffix="o'clock"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                    <v-text-field
+                        v-model="wd.wd_undermining"
+                        label="Undermining"
+                        suffix="o'clock"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="3">
+                    <div class="subtitle-1">Discharge/Drainage</div>
+                </v-col>
+                <v-col cols="12" sm="6" md="9">
                     <v-combobox
-                        v-model="wd.wd1"
-                        :items="['Patient lives alone',
-                                'Patient lives with other person(s) in the home',
-                                'Patient lives in conregate situation (e.g. assissted living)']"
-                        label="Patient Living Situation: Which of the following best describes the patient's residential circumstances and availability of assisstance?"
+                        v-model="wd.wd_discharge"
+                        :items="['None', 'Minimal', 'Moderate', 'Excessive/Profuse']"
+                        label=""
                         single    
                         chips
                     ></v-combobox>
+                </v-col>
+                <v-col cols="12" sm="12" md="12" >
+                    <v-text-field
+                        v-model="wd.wd_color"
+                        label="Color"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" md="12" sm="12"><v-divider></v-divider></v-col>
+                <v-col cols="12" sm="4" md="4">
+                    <div class="subtitle-1">Smell</div>
+                </v-col>
+                <v-col cols="12" sm="4" md="4">
+                    <v-combobox
+                        v-model="wd.wd_smell"
+                        :items="['None', 'Yes']"
+                        label=""
+                        single    
+                        chips
+                    ></v-combobox>
+                </v-col>
+                <v-col cols="12" sm="4" md="4" >
+                    <v-text-field
+                        v-model="wd.wd_describe"
+                        label="Describe"
+                        required>
+                    </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="6" >
+                    <div class="subtitle-1">*Documentation may include (weekly) photograph of the wound and included in the record</div>
                 </v-col>
             </v-row>
         </v-container>
