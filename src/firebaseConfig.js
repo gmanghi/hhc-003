4cd2d7e8ab3b2718b117c18dd5e1336222b01ba0
Glenn Manghi <gmanghi@gmail.com>
2019-11-26 22:37:39 +0800

    fix conflict

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 // Staging Configuration
 export default {
+<<<<<<< .merge_file_a12992
     apiKey: "AIzaSyC8UwonfBrVGfoLMUKyQ_Z-_DJqLv4PZFU",
     authDomain: "hhc-ph-cm-staging.firebaseapp.com",
     databaseURL: "https://hhc-ph-cm-staging.firebaseio.com",
@@ -7,4 +8,14 @@ export default {
     storageBucket: "hhc-ph-cm-staging.appspot.com",
     messagingSenderId: "842427870887",
     appId: "1:842427870887:web:224e3f41d4805c252a3582"
+=======
+    apiKey: "AIzaSyAsfBA0RjtGuJs_KR1nzna8H5E4z6r0b6o",
+    authDomain: "hhc-ph-case-management.firebaseapp.com",
+    databaseURL: "https://hhc-ph-case-management.firebaseio.com",
+    projectId: "hhc-ph-case-management",
+    storageBucket: "hhc-ph-case-management.appspot.com",
+    messagingSenderId: "1034542419709",
+    appId: "1:1034542419709:web:7d7b37753e6edcf5bd7894",
+    measurementId: "G-2FLM1FERLZ"
+>>>>>>> .merge_file_a01204
 }
